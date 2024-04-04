6daab318f11e4ee5727a7207a55d81f97bcd810d
praisex64 <essiakpevwepraise@gmail.com>
2024-04-04 01:47:41 -0700

    setup-script

--- console/console.py
+++ console/console.py
@@ -2,14 +2,14 @@
 """ Console Module """
 import cmd
 import sys
-from models.base_model import BaseModel
-from models.__init__ import storage
-from models.user import User
-from models.place import Place
-from models.state import State
-from models.city import City
-from models.amenity import Amenity
-from models.review import Review
+from ..models.base_model import BaseModel
+from ..models.__init__ import storage
+from ..models.user import User
+from ..models.place import Place
+from ..models.state import State
+from ..models.city import City
+from ..models.amenity import Amenity
+from ..models.review import Review
 
 
 class HBNBCommand(cmd.Cmd):
@@ -326,6 +326,7 @@ class HBNBCommand(cmd.Cmd):
         """ Help information for the update class """
         print("Updates an object with new information")
         print("Usage: update <className> <id> <attName> <attVal>\n")
+
 
 if __name__ == "__main__":
     HBNBCommand().cmdloop()
